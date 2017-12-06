9b689db697325dad0de3f9fc241228ffc96a88cd
Shoblik <shoblik@yahoo.com>
2017-12-06 12:54:33 -0800

    initial commit to save work on new branch

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('App working: open app/index.js to start exploring ES6');
+
+import * as math from './calc';
+console.log('math:', math);
+
+console.log('5+7 =', math.default(5,7));
+
+console.log('8-7 = ', math.subtract(8,7));
+
+console.log('9 x 9 = ', math.multiply(9,9));
